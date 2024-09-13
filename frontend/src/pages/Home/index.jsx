e224a2e65fe6963fbd7bc07c39c313cef78fd1f3
Renan Albuquerque <renan.albuquerque2501@gmail.com>
2024-09-13 19:19:27 -0300

    log adicionado

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -22,6 +22,7 @@ const Home = () => {
     const response = await getUserServer();
 
     if (response.ok) {
+      console.log(response.content);
       setDadosUser(response.content);
     } else {
       console.error(response);
